c330a7bdd3de4bcb802bbf0bcb93e325e2b90b1f
MellowYarker <milanmiladinovic@outlook.com>
2020-07-03 14:47:56 -0400

    Added default mapbox style and api key var
        - these can be modified in config.js
        - cleaned up testing.html

--- public_html/config.js
+++ public_html/config.js
@@ -32,3 +32,6 @@ SiteCircles = true; // true or false (Only shown if SiteShow is true)
 // In nautical miles or km (depending settings value 'Metric')
 SiteCirclesDistances = new Array(100,150,200);
 
+
+mapboxgl.accessToken = ''; // your mapbox API key here
+MapStyle = 'mapbox://styles/mapbox/light-v10'; // default map style
