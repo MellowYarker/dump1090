dd092e37aba9745235701ebd1a04758760dcd8c0
MellowYarker <milanmiladinovic@outlook.com>
2020-07-14 12:25:27 -0400

    Added trailing line when plane is selected.
        - removed a bunch of unused functions
        - modified planeObject, new properties
        - modified most functions

--- public_html/newPlane.js
+++ public_html/newPlane.js
@@ -25,77 +25,81 @@ var planeObject = {
 	vPosition	: false,
 	vTrack		: false,
 
-	// GMap Details
-	// TODO (MILAN): We can use this for mapbox too.
-	/**	marker would be the actual mapbox marker object
-	 *
-	 */
+	// Map Details
+	// marker will be an actual mapbox marker object
 	marker		: null,
 	marked		: false, // true if mapbox marker exists for this plane, else false
 	markerColor	: MarkerColor,
-	lines		: [],
-	trackdata	: new Array(),
-	trackline	: new Array(),
+	line		: {
+					"type": "geojson",
+					"data": {
+						"type": "Feature",
+						"geometry": {
+							"type": "LineString",
+							"coordinates": []
+						},
+						"properties": {}
+					}
+				},
+	sourceID    : null, // name of the source + layer for geoJSON line representing plane tail
+	trackdata   : [],
+	trackline   : [], // coordinates for plane tail line
 
 	// When was this last updated?
 	updated		: null,
 	reapable	: false,
 
+
+	// Update our planes tail line,
+	// TODO: Make this multi colored based on options
+	//		altitude (default) or speed
+	// (milan): we can do this if we make the line an array of geoJSON objects.
+	funcUpdateLines: function() {
+            // update our cooridinates array
+            this.line.data.geometry.coordinates = this.trackline;
+
+            // either update or create the line source
+            if (map.getSource(this.sourceID)) {
+                (map.getSource(this.sourceID)).setData(this.line.data);
+            } else {
+                map.addSource(this.sourceID, this.line);
+            }
+        },
+
 	// Appends data to the running track so we can get a visual tail on the plane
 	// Only useful for a long running browser session.
 	funcAddToTrack	: function(){
-			// TODO: Write this function out
 			this.trackdata.push([this.longitude, this.latitude, this.altitude, this.track, this.speed]);
-			this.trackline.push(new mapboxgl.LngLat(this.longitude, this.latitude));
+			this.trackline.push([this.longitude, this.latitude]);
 		},
 
 	// This is to remove the line from the screen if we deselect the plane
 	funcClearLine	: function() {
-			// if (this.line) {
-			// 	this.line.setMap(null);
-			// 	this.line = null;
-			// }
-		},
+            // only try to remove if a layer is found
+            if (map.getLayer(this.sourceID)) {
+                this.is_selected = false;
+                map.removeLayer(this.sourceID);
+            }
+        },
 
-	// Should create an icon for us to use on the map...
-	// TODO (MILAN): do we need to do anything? Our icon is just a PNG and we can
-	// 		 control the rotation when we set the icon.
-	funcGetIcon		: function() {
-			this.markerColor = MarkerColor;
-			// If this marker is selected we should make it lighter than the rest.
-			if (this.is_selected == true) {
-				this.markerColor = SelectedColor;
-			}
-
-			// If we have not seen a recent update, change color
-			if (this.seen > 15) {
-				this.markerColor = StaleColor;
-			}
-
-			// If the squawk code is one of the international emergency codes,
-			// match the info window alert color.
-			if (this.squawk == 7500) {
-				this.markerColor = "rgb(255, 85, 85)";
-			}
-			if (this.squawk == 7600) {
-				this.markerColor = "rgb(0, 255, 255)";
-			}
-			if (this.squawk == 7700) {
-				this.markerColor = "rgb(255, 255, 0)";
-			}
-
-			// If we have not overwritten color by now, an extension still could but
-			// just keep on trucking.  :)
-
-			// return {
-                // anchor: new google.maps.Point(32, 32), //anchor to middle of plane.
-                // rotation: this.track
-            // };
-		},
-
-	// TODO: Trigger actions of a selecting a plane
-	funcSelectPlane	: function(selectedPlane){
-			selectPlaneByHex(this.icao);
+	// update and display the planes tail line
+	funcSelectPlane	: function() {
+            this.is_selected = true;
+			this.funcUpdateLines();
+            // add the layer to the map
+            map.addLayer({
+                "id"    :  	this.sourceID,
+                "type"  : "line",
+                "source": this.sourceID,
+                "layout": {
+                    "line-join" : "round",
+                    "line-cap"  : "round",
+                },
+                "paint": {
+                    "line-color"   : "#e96c51",
+                    "line-width"    : 5
+                }
+            });
 		},
 
 	// Update the plane data that we recieve from the geoJSON response.
@@ -120,6 +124,10 @@ var planeObject = {
 			this.messages	= updateProperty.messages;
 			this.seen	= updateProperty.seen;
 
+            // set the sourceID for the line
+            if (!this.sourceID && this.icao) {
+                this.sourceID = this.icao + "-line";
+            }
 			// If no packet in over 58 seconds, consider the plane reapable
 			// This way we can hold it, but not show it just in case the plane comes back
 			if (this.seen > 58) {
@@ -128,21 +136,17 @@ var planeObject = {
 					this.marker.remove();
 					this.marked = false;
 				}
-				// TODO (MILAN): Come back to this when we can draw lines!
-				// if (this.line) {
-				// 	this.line.setMap(null);
-				// 	this.line = null;
-				// }
-				if (SelectedPlane == this.icao) {
-					if (this.is_selected) {
-						this.is_selected = false;
-					}
-					SelectedPlane = null;
+				if (map.getSource(this.sourceID)) {
+					this.line.data.geometry.coordinates = [];
+					this.trackline = [];
+					this.trackdata = [];
+                    this.funcClearLine();
+                    // TODO (milan): we might not want to fully remove the source yet
+					// map.removeSource(this.sourceID);
 				}
 			} else {
-				// TODO (MILAN): this doesn't do anything, what could we have it do?
-				if (this.reapable == true) {
-				}
+				// TODO (MILAN): is this a plane that has returned from a reaped state?
+				if (this.reapable == true) {}
 				this.reapable = false;
 			}
 
@@ -164,14 +168,11 @@ var planeObject = {
 					changeAlt = true;
 				}
 				// Right now we only care about lat/long, if alt is updated only, oh well
-				// TODO (MILAN): if we add 3D, we will want to modify this!
+				// TODO (MILAN): modify this if we add 3D or line colour that depends on alt
 				if ((changeLat == true) || (changeLon == true)) {
 					this.coord_change = true;
 					this.funcAddToTrack();
-					// TODO (MILAN): another line situation.
-					if (this.is_selected) {
-						this.line = this.funcUpdateLines();
-					}
+					this.funcUpdateLines(); // will only display change if selected
 				} else {
 					this.coord_change = false;
 				}
@@ -218,7 +219,7 @@ var planeObject = {
 					}
 					const elapsed = timestamp - start;
 
-					let duration = 0.94; // duration of the animation in seconds, subject for review.
+					let duration = 0.999; // duration of the animation in seconds, subject for review.
 					let x, y;
 					// Transition from old pos to new pos in a line.
 					// y = m(x - x_0) + y_0,   where *_0 is old position coord.
@@ -281,7 +282,9 @@ var planeObject = {
 				this.marker = new mapboxgl.Marker(el)
 					.setLngLat([this.longitude, this.latitude])
 					.setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-					.setHTML('<h3>' + this.callsign.length === 0 ? this.hex : this.callsign + ' ('+ this.icao + ')' + '</h3><p>' + 'add something here' + '</p>')) // popup content
+					.setHTML('<h3>' + this.callsign.length === 0 ? this.hex : this.callsign + ' ('+ this.icao + ')' + '</h3><p>' + 'add something here' + '</p>')
+                    .on('open', this.funcSelectPlane.bind(this)) // plane has been selected
+                    .on('close', this.funcClearLine.bind(this))) // plane has been deselected
 					.addTo(map);
 
 				this.marked = true;
@@ -289,28 +292,6 @@ var planeObject = {
 
 			this.marker.setRotation(this.track); // rotate the plane icon
 
-				// Trap clicks for this marker.
-				// TODO (MILAN): we want to do something similar!
-				// google.maps.event.addListener(this.marker, 'click', this.funcSelectPlane);
 			return this.marker;
-		},
-
-	// Update our planes tail line,
-	// TODO: Make this multi colored based on options
-	//		altitude (default) or speed
-	funcUpdateLines: function() {
-			// if (this.line) {
-			// 	var path = this.line.getPath();
-			// 	path.push(new google.maps.LatLng(this.latitude, this.longitude));
-			// } else {
-			// 	this.line = new google.maps.Polyline({
-			// 		strokeColor: '#000000',
-			// 		strokeOpacity: 1.0,
-			// 		strokeWeight: 3,
-			// 		map: GoogleMap,
-			// 		path: this.trackline
-			// 	});
-			// }
-			return this.line;
-		}
+	}
 };
